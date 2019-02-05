440f146ba0633e1fa4f7cea32474e6355b7288c4
Lucy Suddenly <lucysuddenly@Lucys-Air.local.tld>
2019-02-04 18:48:53 -0800

    done

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,54 @@ def reload
   load 'config/environment.rb'
 end
 
+# l1 = Listing.new("Seattle")
+# l2 = Listing.new("Seattle")
+# l3 = Listing.new("Seattle")
+# l4 = Listing.new("Seattle")
+# l5 = Listing.new("Seattle")
+# l6 = Listing.new("Seattle")
+# l7 = Listing.new("Seattle")
+#
+# g1 = Guest.new("Al")
+# g2 = Guest.new("Bob")
+# g3 = Guest.new("Ted")
+# g4 = Guest.new("Susan")
+# g5 = Guest.new("Lucy")
+# g6 = Guest.new("Barbara")
+# g7 = Guest.new("Steve")
+# g8 = Guest.new("Edna")
+#
+# t1 = Trip.new(l1, g1)
+# t2 = Trip.new(l2, g2)
+# t3 = Trip.new(l3, g3)
+# t4 = Trip.new(l4, g4)
+# t5 = Trip.new(l5, g5)
+# t6 = Trip.new(l6, g6)
+# t7 = Trip.new(l7, g7)
+# t8 = Trip.new(l7, g8)
+# t9 = Trip.new(l1, g1)
+
+# b1 = Bakery.new
+# b2 = Bakery.new
+
+# d1 = Dessert.new(b1)
+# d2 = Dessert.new(b1)
+# d3 = Dessert.new(b2)
+# d4 = Dessert.new(b2)
+# d5 = Dessert.new(b2)
+
+# i1 = Ingredient.new(rand(100..1000), d1, "flour")
+# i2 = Ingredient.new(rand(100..1000), d2, "chocolate")
+# i3 = Ingredient.new(rand(100..1000), d3, "sugar")
+# i4 = Ingredient.new(rand(100..1000), d4, "cocoa")
+# i5 = Ingredient.new(rand(100..1000), d5, "vanilla")
+# i6 = Ingredient.new(rand(100..1000), d1, "powdered sugar")
+# i7 = Ingredient.new(rand(100..1000), d2, "baking powder")
+# i8 = Ingredient.new(rand(100..1000), d3, "baking soda")
+# i9 = Ingredient.new(rand(100..1000), d4, "water")
+# i10 = Ingredient.new(rand(100..1000), d5, "egg")
+
+
+
+
 Pry.start
